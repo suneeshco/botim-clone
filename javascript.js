f00eef8e662ebbd07344dad59d55e9fe9e8f74b0
suneeshco <137856056+suneeshco@users.noreply.github.com>
2023-07-06 18:01:55 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var a=document.getElementById("link");
+document.getElementById("he").addEventListener("click",explain)
+function explain()
+{
+    a.innerHTML="CLICK ME";
+}
+function message()
+{
+    alert("Select your preffered language");
+}
